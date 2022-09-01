fb26ff4122a18e127d5343b5a0a3d1de5fc68cab
dawnpoem <dawnpoem@naver.com>
2022-09-01 18:46:17 +0900

    js_clone_1

--- app.js
+++
@@ -1,8 +0,0 @@
-const loginForm = document.querySelector("#login-form");
-
-function onLoginSubmit() {
-  const username = loginInput.value;
-  console.log(username);
-}
-
-loginForm.addEventListener("submit", onLoginSubmit);
